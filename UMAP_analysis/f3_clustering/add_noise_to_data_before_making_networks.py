3a08a99978492831b31602c81a9bf3321d13e9be
Joel Angus Hancock <jhancock@cemm.at>
2021-01-27 11:17:36 +0100

    unknown unsaved changes

--- UMAP_analysis/f3_clustering/add_noise_to_data_before_making_networks.py
+++ UMAP_analysis/f3_clustering/add_noise_to_data_before_making_networks.py
@@ -1,31 +1,17 @@
-import pandas as pd
-
 import umap
-
-import numpy as np
 import numba
+import numpy as np
+import pandas as pd
 import networkx as nx
 
-import os
-os.chdir('/Users/jhancock/UMAP_analysis/')
-
-
-def umap_network(df, nn):
-    rndstate = np.random.RandomState()
-    knn_net = umap.umap_.fuzzy_simplicial_set(df.values, nn, rndstate, 'manhattan')
-    G = nx.from_numpy_array(knn_net[0].toarray(), create_using = nx.DiGraph)
-    num_to_id = dict(enumerate(df.index))
-    return nx.relabel_nodes(G, num_to_id.get)
-
-import os
-os.chdir('//')
-
+from UMAP_analysis.f2_build_networks.make_networks import umap_network
 
 n = 14
-for dataset in ['GDSC']:
-    print()
+for dataset in ['toy']:
     df = pd.read_csv(f"munged_data/{dataset}/features.csv", index_col=0)
     deviations = df.std()
     for scaling_factor in np.linspace(0,2,5):
         G = umap_network(df+scaling_factor*deviations, n)
-        nx.write_gml(G, f"3.clustering/noised_datasets/{dataset}/similarity_with_noise_level_{str(scaling_factor)}.gml")
+        pdb.set_trace()
+        nx.write_gml(G, f"f3_clustering/noised_datasets/{dataset}/similarity_with_noise_level_{scaling_factor}.gml")
+
