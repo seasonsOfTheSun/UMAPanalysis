b82015f270d90f85835ad50f6f5cd37810b013c1
Joel Angus Hancock <jhancock@cemm.at>
2021-02-03 11:34:58 +0100

    Move key module functions to a core module

--- UMAP_analysis/core.py
+++ UMAP_analysis/core.py
@@ -1,27 +1,32 @@
+import sys
 import umap
 import numpy as np
 import pandas as pd
 import networkx as nx
+import scipy.sparse
+
 
 def umap_network(df, nn):
+    """ """
     rndstate = np.random.RandomState()
     knn_net = umap.umap_.fuzzy_simplicial_set(df.values, nn, rndstate, 'manhattan')
     G = nx.from_numpy_array(knn_net[0].toarray(), create_using = nx.DiGraph)
     num_to_id = dict(enumerate(df.index))
     return nx.relabel_nodes(G, num_to_id.get)
 
-def scaled_laplacian(G):
-    components = nx.weakly_connected_component_subgraphs(G)
-    G = max(components, key = lambda H: H.order())
-    nodes = G.nodes()
+def scaled_laplacian(G, nodes = None):
+    if nodes == None:
+        nodes = list(G.nodes())
 
     A = nx.adjacency_matrix(G)
     temp = G.out_degree(weight = 'weight')
-    scaled = scipy.sparse.diags([1/temp[i] for i in nodes]) * A
+    degree = [temp[i] for i in nodes]
+    scaling =  scipy.sparse.diags([1/i if i!=0 else 0 for i in degree])
+    scaled = scaling * A
     return scaled
 
-def eigenvalues(scaled):
-    eval_, evec = scipy.sparse.linalg.eigs(scaled, k = 1000)
+def eigenvalues(scaled, nodes, k = 10):
+    eval_, evec = scipy.sparse.linalg.eigs(scaled, k = k)
 
     eval_ = np.real(eval_)
     evec = np.real(evec)
@@ -31,3 +36,47 @@ def eigenvalues(scaled):
     evec.loc["lambda",:] = eval_
     evec[np.argsort(eval_)[::-1]]
     return evec
+
+def make_undirected(G):
+    out = []
+    G_u = G.to_undirected()
+    for edge in G_u.edges():
+        i,j = edge
+        try:
+            w = G[(j,i)]['weight']
+            G_u[(i,j)]['weight'] += w
+        except KeyError:
+            pass
+    return G_u
+
+def propagate(propagator, nodes, labels):
+    out = []
+    out_time = []
+    for i,x in enumerate(labels.columns):
+        start = time.time()
+        v = scipy.sparse.csc_matrix(labels.loc[list(nodes), x].values.reshape(-1, 1))
+        temp = pd.Series((propagator * v).toarray().ravel())
+        temp.name = x
+        out.append(temp)
+        stop = time.time()
+        out_time.append(stop-start)
+
+    df = pd.concat(out, axis=1)
+    df.index = nodes
+    df_time = pd.Series(out_time, name="Time", index=labels.columns)
+
+    return df, df_time
+
+
+def nearest_neighbor(G,labels):
+    out = {}
+    for node in G.nodes():
+        neighbors = list(G[node].keys())
+        neighbor_labels = labels.loc[neighbors]
+        if len(known_neighbors) > 0:
+            nearest_known_neighbor = max(neighbors, key=lambda x: similarity[node][x]['weight'])
+            prediction = labels.loc[nearest_known_neighbor]
+        else:
+            prediction = 0
+        out[node] = prediction
+    return out
