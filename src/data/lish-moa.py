8877d4a354dc654319719476fe13748351589690
Joel Angus Hancock <jhancock@cemm.at>
2021-02-08 20:40:21 +0100

    fidgeted with the whitespace

--- src/data/lish-moa.py
+++ src/data/lish-moa.py
@@ -1,20 +1,16 @@
-
 import pandas as pd
 import numpy as np
 
-import os
-os.chdir('//')
 
-#df = pd.read_csv("data/raw/lish-moa/sample_submission.csv", index_col = 0)
-X_unclassified = pd.read_csv("data/raw/lish-moa/test_features.csv", index_col = 0);
-X_classified = pd.read_csv("data/raw/lish-moa/train_features.csv", index_col = 0);
-X = pd.concat([X_classified, X_unclassified]);
-
+X_unclassified = pd.read_csv("data/raw/lish-moa/test_features.csv", index_col = 0)
+X_classified = pd.read_csv("data/raw/lish-moa/train_features.csv", index_col = 0)
+X = pd.concat([X_classified, X_unclassified])
 
 
 y_classified = pd.read_csv("data/raw/lish-moa/train_targets_scored.csv", index_col = 0)
 MoAs = y_classified.columns
 known = pd.Series({i:1 for i in y_classified.index})
+
 
 y_unclassified = np.zeros((len(X_unclassified.index), len(MoAs)))
 y_unclassified = pd.DataFrame(y_unclassified)
@@ -22,15 +18,15 @@ y_unclassified.columns = MoAs
 y_unclassified.index= X_unclassified.index
 temp = pd.Series({i:0 for i in y_unclassified.index})
 known  = pd.concat([known, temp])
-
 y = pd.concat([y_classified, y_unclassified]);
+
 
 metadata_columns = ["cp_type", "cp_time", "cp_dose"]
 metadata = X[metadata_columns]
 metadata["known"] = known
+
+
 features = X[[i for i in X.columns if i not in metadata_columns]]
-
-
 features_unscaled = features.copy()
 features = features_unscaled / features_unscaled.std(axis = 0)
 
