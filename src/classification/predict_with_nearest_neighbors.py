ce7e2becf723a3676c5de67f14d5e5b5dbb503c3
Joel Angus Hancock <jhancock@cemm.at>
2021-02-08 10:13:40 +0100

    Add some more MoA prediction Methods

--- src/classification/predict_with_nearest_neighbors.py
+++ src/classification/predict_with_nearest_neighbors.py
@@ -1,21 +1,26 @@
 import networkx as nx
+import pandas as pd
 import sys
 import re
 
-from UMAP_analysis.core import nearest_neighbors
+from UMAP_analysis.core import nearest_neighbor
 
 filename = sys.argv[1]
-m = re.match("networks/{dataset}_similarity.*?.gml", filename)
+m = re.match("networks/(?P<dataset>.*?)_similarity.*?.gml", filename)
 dataset = m.groupdict()['dataset']
 G = nx.read_gml(filename)
 
-true_labels = pd.read_csv("data/intermediate/{dataset}/labels.csv", index_col = 0)
-train = pd.read_csv("data/intermediate/{dataset}/train.csv",index_col = 0)
-train = train.reindex(true_labels.index)
+true_labels = pd.read_csv(f"data/intermediate/{dataset}/labels.csv", index_col = 0)
+train = pd.read_csv(f"data/intermediate/{dataset}/train.csv", header = None)[0].values
+train = set(train)
 
+mask = [i in train for i in true_labels.index]
+mask = pd.Series(mask, index = true_labels.index)
+labels = true_labels.mask(~mask).fillna(0)
 
 out = []
 for moa in labels.columns:
     predictions = nearest_neighbor(G, labels[moa])
     out.append(predictions)
-pd.concat(out, axis = 1).to_csv("data/intermediate/predictions/"+filename.split("/")[-1].split(".")[0]+")
+    break
+pd.concat(out, axis = 1).to_csv("data/intermediate/predictions/"+filename.split("/")[-1].split(".")[0]+".csv")
--- src/classification/predict_with_random_forest.py
+++ src/classification/predict_with_random_forest.py
@@ -1,9 +1,8 @@
+import pandas as pd
+import sys
+import sklearn.ensemble as se
 
-features = pd.read_csv(f"data/intermediate/{dataset}/features_train.csv", index_col = 0)
-classes  = pd.read_csv(f"data/intermediate/{dataset}/moas_train.csv", index_col = 0)
-metadata = pd.read_csv(f"data/intermediate/{dataset}/metadata.csv", index_col = 0)
-metadata["known"] = metadata.known.astype('bool')
-
+dataset = sys.argv[1]
 
 features = pd.read_csv(f"data/intermediate/{dataset}/features.csv", index_col = 0)
 labels = pd.read_csv(f"data/intermediate/{dataset}/labels.csv", index_col = 0)
@@ -17,15 +16,15 @@ features_train = features.loc[train]
 features_test = features.loc[test]
 training_labels = labels.loc[train]
 
-
-out = []
-rg = se.RandomForestRegressor(n_estimators=10)
-for moa in labels.columns:
+df = []
+for moa in training_labels.columns:
+    rg = se.RandomForestRegressor(n_estimators=10)
     rg.fit(features_train, training_labels[moa])
-    t = pd.Series(rg.predict(features_test))
+    out = pd.Series(rg.predict(features_test))
+    out.index = features_test.index
+    out.name = moa
+    df.append(out)
 
-    t.index = features_test.index
-    t.name = x
-    out.append(t)
 
-pd.concat(axis = 1).to_csv(f"data/processed/predictions/{dataset}_random_forest.csv")
+df = pd.concat(df, axis=1)
+df.to_csv(f"data/processed/predictions/{dataset}/random_forest_predictions.csv")
