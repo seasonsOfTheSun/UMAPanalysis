6e37f968a404feaf4235c3a317ba7b316f874287
joel.hancock <joel.hancock@clip-login-0.cbe.vbc.ac.at>
2021-04-06 16:00:22 +0200

    compute clusters using the OPTICS algorithm

---
+++ src/clustering/optics.py
@@ -0,0 +1,22 @@
+import sys
+import pandas as pd
+import networkx as nx
+import numpy as np
+
+
+
+dataset = sys.argv[1]
+noise_percent = sys.argv[2]
+
+
+df = pd.read_csv(f"data/intermediate/{dataset}/features.csv", index_col=0)
+deviations = df.std()
+scaling_factor = int(noise_percent)/100
+df = df+scaling_factor*np.random.randn(*df.shape)
+
+
+import sklearn.cluster
+model = sklearn.cluster.OPTICS()
+clusters = model.fit_predict(df)
+clusters = pd.Series(clusters, index = df.index)
+clusters.to_csv(f"data/processed/clusters/{dataset}/optics_noise_percent_{noise_percent}.csv", header = None)
