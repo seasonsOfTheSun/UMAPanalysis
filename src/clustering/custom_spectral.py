bceaba9e3c842ffa188bb98b0c24626782a92731
Joel Hancock <joel@menchelab.com>
2021-04-28 17:26:12 +0200

    Spectral clustering with the data map network

--- src/clustering/custom_spectral.py
+++ src/clustering/custom_spectral.py
@@ -7,7 +7,7 @@ from UMAP_analysis.core import scaled_laplacian_opposite, eigenvalues
 filename = sys.argv[1]
 n_clusters = 40
 n_dims = 10
-expr = "networks/(?P<dataset>.*?)/(?P<name>.*?).gml"
+expr = "networks/(?P<dataset>.*?)/noisy/(?P<name>.*?).gml"
 m = re.match(expr, filename)
 dataset = m.groupdict()['dataset']
 name = m.groupdict()['name']
@@ -22,6 +22,6 @@ evecs = eigenvalues(laplacian, nodes)
 
 
 import sklearn.cluster
-_,clusters,_ = sklearn.cluster.k_means(evecs.iloc[:-1,1:n_dims],n_clusters)
-clusters = pd.Series(clusters, index = evecs.index)
+_,clusters,_ = sklearn.cluster.k_means(evecs.iloc[:-1,:n_dims+1],n_clusters)
+clusters = pd.Series(clusters, index = evecs.index[:-1])
 clusters.to_csv(f"data/processed/clusters/{dataset}/custom_spectral_n_clusters_{n_clusters}_{name}.csv", header = None)
